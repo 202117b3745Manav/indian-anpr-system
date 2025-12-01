36366f0d5dd32740364dddeab5c71f549f759697
MANAVVASHISTHA <148677229+MANAVVASHISTHA@users.noreply.github.com>
2025-12-01 21:35:49 +0530

    commit for adding OpenCV dependency and testing its usage

--- src/main/java/com/anpr/App.java
+++ src/main/java/com/anpr/App.java
@@ -1,13 +1,18 @@
 package com.anpr;
 
+import org.opencv.core.Core;
+import nu.pattern.OpenCV;
+
 /**
- * Hello world!
- *
+ * Main application class to test OpenCV setup.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Loads the OpenCV native library.
+        OpenCV.loadLocally();
+
+        // Print the OpenCV version if loaded correctly.
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        System.out.println("Success! OpenCV native library loaded correctly.");
     }
 }
